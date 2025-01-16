fb1c775e038ac1eeb1a313a01d8d2a7c333e313b
saran <spskartist@gmail.com>
2025-01-16 23:57:10 +0530

    minor code changes

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # ideabot
 Idea suggestion chatbot with openai
+
+## Installation and setup
+### Virtual environment setup
+- Create Virtual Environment
+```
+virtualenv venv -p path/to/python_version
+```
+- Start Virtual Environment
+```
+source venv/bin/activate
+```
+
+### Install python packages
+```
+pip install flask openai
+```
+
+### Add api key
+- Copy the ```.evn.sample``` and save it as .env
+- Paste the api key in the space mentioned
+
+## Luanch the app
+```
+flask run
+```
+- Visit <link>http://127.0.0.1:5000</link> to view the project 
--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, jsonify
 from openai import OpenAI
+from dotenv import load_dotenv
 import os
 
 app = Flask(__name__)
+load_dotenv()
 
 client = OpenAI(
     api_key=os.getenv("OPENAI_API_KEY")
@@ -23,15 +25,33 @@ def get_idea():
             messages=[
                 {"role": "system", "content": "You are a helpful assistant."},
                 {"role": "user", "content": f"Generate 3 unique ideas for: {query} such that the content is a list of\
-                 ideas and nothing else. Each idea contains not more than 10 words. The ideas must be short and crisp.\
-                 The content must not include descriptions."}
+                 ideas and nothing else. These ideas must must be software engineering related. Each idea contains not\
+                 more than 10 words. The ideas must be short and crisp. The content must not include descriptions."}
             ],
             max_tokens=100,
         )
-        idea = response
-        print(idea)
-        idea = response.choices[0].message.content.strip()
+        idea = response.choices[0].message.content.strip().split("\n")
+        print(idea, type(idea))
         return jsonify({'idea': idea})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+
+@app.route('/get_detailed_description', methods=['POST'])
+def get_detailed_description():
+    options = request.json.get("options")
+    try:
+        response = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": f"Elaborate the following ideas {options} in detail such that the contents does not\
+                 exceed 100 words per idea and nothing else."}
+            ],
+            max_tokens=100,
+        )
+        idea_description = response.choices[0].message.content
+        return jsonify({'detailed_description': idea_description})
     except Exception as e:
         return jsonify({'error': str(e)})
 
